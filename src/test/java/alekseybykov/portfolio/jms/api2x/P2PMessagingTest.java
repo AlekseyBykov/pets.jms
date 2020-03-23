746baf16bfa87196eacbc7d03ec260a7d94cd2f4
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-03-23 06:42:18 +0400

    Added test for example of using P2P messaging model with using JMS 2.X API.

---
+++ src/test/java/alekseybykov/portfolio/jms/api2x/P2PMessagingTest.java
@@ -0,0 +1,48 @@
+package alekseybykov.portfolio.jms.api2x;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
+import javax.jms.Queue;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import static org.junit.Assert.assertEquals;
+
+public class P2PMessagingTest {
+
+	private static final String queueName = "queue/jmsQueue";
+
+	private static InitialContext initialContext;
+	private static Queue queue;
+
+	@BeforeClass
+	public static void setup() {
+		try {
+			initialContext = new InitialContext();
+			queue = (Queue) initialContext.lookup(queueName);
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testSendTextMessageToQueueAndThenReceive() {
+		try (ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
+		    JMSContext jmsContext = activeMQConnectionFactory.createContext()) {
+			String message = "message";
+
+			JMSProducer jmsProducer = jmsContext.createProducer();
+			jmsProducer.send(queue, message);
+
+			JMSConsumer jmsConsumer = jmsContext.createConsumer(queue);
+			String receivedMessage = jmsConsumer.receiveBody(String.class);
+
+			assertEquals(message, receivedMessage);
+		}
+	}
+}
